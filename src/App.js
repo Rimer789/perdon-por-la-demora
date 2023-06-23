61db1d1ef2e7a3602c3e2698003b7f10c2bd0239
rimer rosa herrera <201809665@est.umss.edu>
2023-06-23 00:20:08 -0400

    pues

--- src/App.js
+++ src/App.js
@@ -1,63 +1,48 @@
 import React, { useState } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes, Outlet } from 'react-router-dom';
-import Sidebar from './components/navegacion/sidebar.js';
-import Panel from './components/panelprincipal/panel.js';
-import Espacios from './components/espacio/espacio.js';
-import Login from './components/login/login.js';
-import Register from './components/registro/Register.js';
-import Acceso from './components/accesos/acceso.js';
-import Cliente from './components/clientes/cliente.js';
-import Cobro from './components/cobros/cobro.js';
-import Deuda from './components/deudas/deuda.js';
-import Guardia from './components/guardias/guardia.js';
-import Reclamo from './components/reclamos/reclamo.js';
-import Reporte from './components/reportes/reporte.js';
-import Vehiculo from './components/vehiculos/vehiculo.js';
-import Misvehiculos from './components/misVehiculos/misvehiculos.js';
-import Historial from './components/historial/historial.js';
-import IngresoSalida from './components/ingresoSalida/ingresosalida.js';
-import Reservar from './components/reservar/reservar.js';
-import Turno from './components/turno/turno.js'
+
 
 const App = () => {
-  const [userRole, setUserRole] = useState(null);
+  const [isAccepted, setIsAccepted] = useState(null);
+
+  const handleYesClick = () => {
+    window.open('https://wa.me/59167566295?text=si%20iremos :) %20', '_blank');
+    setIsAccepted(true);
+  };
+
+  const handleNoMouseOver = () => {
+    const randomX = parseInt(Math.random() * 100);
+    const randomY = parseInt(Math.random() * 100);
+    const noBtn = document.querySelector('#noBtn');
+    noBtn.style.setProperty('top', `${randomY}%`);
+    noBtn.style.setProperty('left', `${randomX}%`);
+    noBtn.style.setProperty('transform', `translate(-${randomX}%, -${randomY}%)`);
+  };
+
+  
 
   return (
-    <Router>
-      <Routes>
-        {userRole !== null ? (
-          <Route
-            path="/"
-            element={
-              <Sidebar userRole={userRole}>
-                <Outlet /> {/* Renderiza las rutas secundarias */}
-              </Sidebar>
-            }
-          >
-            <Route path="/" element={<Panel />} />
-            <Route path="/espacios" element={<Espacios />} />
-            <Route path="/acceso" element={<Acceso />} />
-            <Route path="/cliente" element={<Cliente />} />
-            <Route path="/cobro" element={<Cobro />} />
-            <Route path="/deuda" element={<Deuda />} />
-            <Route path="/guardia" element={<Guardia />} />
-            <Route path="/reclamo" element={<Reclamo />} />
-            <Route path="/reporte" element={<Reporte />} />
-            <Route path="/vehiculo" element={<Vehiculo />} />
-            <Route path="/misvehiculos" element={<Misvehiculos />} />
-            <Route path="/turnos" element={<Turno />} />
-            <Route path="/ingresosalida" element={<IngresoSalida />} />
-            <Route path="/reservar" element={<Reservar />} />
-            <Route path="/historial" element={<Historial />} />
-          </Route>
-        ) : (
-          <Route path="/" element={<Login setUserRole={setUserRole} />} />
-        )}
-        <Route path="/login" element={<Login setUserRole={setUserRole} />} />
-        <Route path="/registro" element={<Register />} />
-      </Routes>
-    </Router>
+    <div className="contenedor">
+      <br />
+      <br />
+      <p>si vamos ir al cine nove? -_- </p>
+      
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <button id="yesBtn" onClick={handleYesClick}>
+        Si
+      </button>
+      <button id="noBtn" onMouseOver={handleNoMouseOver}>
+        No
+      </button>
+      {isAccepted !== null && (
+        <p>Tu respuesta fue: {isAccepted ? 'Sí' : 'No'}</p>
+      )}
+    </div>
   );
 };
 
