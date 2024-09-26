515b11a41c7b37ecd1b035d970107919bdf73551
rimer rosa herrera <201809665@est.umss.edu>
2024-09-25 22:05:54 -0400

    no te salvas

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import vilmaImage from './vilma.jpeg';
+import vilmaImage from './gabi.jpg';
 import backImage from './back.jpeg';
 
 const App = () => {
-  const [isAccepted, setIsAccepted] = useState(null);
+  const [isAccepted, setIsAccepted] = useState(false); // Controla si se ha aceptado
   const [noButtonStyle, setNoButtonStyle] = useState({});
   const [backgroundImage, setBackgroundImage] = useState(vilmaImage);
 
   const handleYesClick = () => {
-    window.open('https://wa.me/59167566295?text=si%20acepto%20%3A)%20', '_blank');
+    // Abrir enlace de WhatsApp
+    window.open('https://wa.me/59167566295?text=si%20vamos%20%3A)%20', '_blank');
+    // Limpiar todo y mostrar mensaje "Ya lo sabía"
     setIsAccepted(true);
   };
 
@@ -37,7 +39,7 @@ const App = () => {
 
   useEffect(() => {
     const handleResize = () => {
-      if (window.innerWidth <= 768) { // Cambia el ancho máximo según tus necesidades
+      if (window.innerWidth <= 768) {
         document.querySelector('.contenedor').classList.add('mobile-center');
       } else {
         document.querySelector('.contenedor').classList.remove('mobile-center');
@@ -54,22 +56,33 @@ const App = () => {
 
   return (
     <div className="contenedor" style={{ backgroundImage: `url(${backgroundImage})` }}>
-      <br />
-      <br />
-      <p>seras mi novia quieras o no </p>
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <button id="yesBtn" onClick={handleYesClick}>
-        Si
-      </button>
-      <button id="noBtn" onTouchStart={handleNoButtonClick} onClick={handleNoButtonClick} style={noButtonStyle}>
-        No
-      </button>
-      {isAccepted !== null && <p> no habia de otra </p>}
+      {isAccepted ? (
+        <h1>Ya lo sabía 😉 solo dime cuando 🙂</h1>
+      ) : (
+        <>
+          <br />
+          <br />
+          <h1>salimos a ver una peli pa el cine🍿🥤🎮</h1>
+          <br />
+          <br />
+          <h1>aver dime que no😑</h1>
+          <br />
+          <br />
+          <br />
+          <br />
+          <button id="yesBtn" onClick={handleYesClick}>
+            Si
+          </button>
+          <button
+            id="noBtn"
+            onTouchStart={handleNoButtonClick}
+            onClick={handleNoButtonClick}
+            style={noButtonStyle}
+          >
+            No
+          </button>
+        </>
+      )}
     </div>
   );
 };
